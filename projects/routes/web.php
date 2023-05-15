2c5406be744716d8d537de9082300917991aadea
Ahmet Tekin <ahmettekin@garantibarter.com.tr>
2023-05-15 12:12:40 +0300

    köklü değişiklik yapıldı

--- projects/routes/web.php
+++ projects/routes/web.php
@@ -1,18 +1,27 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'HomePageController@index')->name('home');
+Route::get('events', 'EventsController@index')->name('events.index');
+Route::get('events/{event:slug}', 'EventsController@show')->name('events.show');
+Route::get('posts/{post:slug}', 'PostsController@show')->name('posts.show');
+
+Route::get('/home', function () {
+    $routeName = auth()->user()->is_blog_writer ? 'admin.posts.index' : 'admin.events.index';
+
+    if (session('status')) {
+        return redirect()->route($routeName)->with('status', session('status'));
+    }
+
+    return redirect()->route($routeName);
 });
